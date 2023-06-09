a5b72efa94157cece38e2ccda96f663c6f2d1099
User <simon.p.behrendt@gmail.com>
2023-06-09 15:42:13 +0200

    Uniaufgabe für uns loesbar gemacht

--- Aufgaben/Day3/volume.c
+++ Aufgaben/Day3/volume.c
@@ -1,12 +1,9 @@
-/*
-Compile: make volume
-Run: ./volume
-make volume && ./volume
-*/
 #define _USE_MATH_DEFINES
-#define _GNU_SOURCE
-#include "base.h"
-
+#include <stdio.h>
+#include<math.h>
+#include <stdbool.h>
+void test_within_d(double a, double b, double epsilon, int line);
+int compare_doubles(double a, double b, double epsilon, int line);
 enum Tag { TCylinder, TSphere, TCuboid };
 typedef enum Tag Tag;
 
@@ -54,9 +51,9 @@ GeomObject make_cuboid(double a, double b, double c) {
 double volume(GeomObject o);
 
 void volume_test(void) {
-    test_within_d(volume(make_sphere(2)), 4 /3.0 * M_PI * 2 * 2 * 2, 1e-6);
-    test_within_d(volume(make_cuboid(2, 3, 4)), 2 * 3 * 4, 1e-6);
-    test_within_d(volume(make_cylinder(3, 4)), 4 * M_PI * 3 * 3 , 1e-6);
+    test_within_d(volume(make_sphere(2)), 4 /3.0 * M_PI* 2 * 2 * 2, 1e-6, __LINE__);
+    test_within_d(volume(make_cuboid(2, 3, 4)), 2 * 3 * 4, 1e-6, __LINE__);
+    test_within_d(volume(make_cylinder(3, 4)), 4 * M_PI * 3 * 3 , 1e-6, __LINE__);
 }
     
 // GeomObject -> double
@@ -72,3 +69,30 @@ int main(void) {
 }
 
 //todo: d)
+void test_i(int a, int b, int line){
+    if(a == b){
+        printf("\nline: %d Test successfull, expected %d got %d", line, b,a);
+    } else {
+        printf("\nline: %d Test failed, expected %d got %d",line, b,a);
+    }
+}
+void test_bool(bool a, bool b, int line){
+    if(a == b){
+        printf("\nline: %d Test successfull, expected %s got %s", line, b? "true":"false", a? "true":"false");
+    } else {
+        printf("\nline: %d Test failed, expected %s got %s", line, b? "true":"false", a? "true":"false");
+    }
+}
+int compare_doubles(double a, double b, double epsilon, int line){
+    if((a-b)>epsilon) return 1;
+    if(((a-b)<epsilon) && ((b-a)<epsilon)) return 0;
+    if((a-b)<epsilon) return -1;
+    return -1;
+}
+void test_within_d(double a, double b, double epsilon, int line){
+    if(compare_doubles(a, b, epsilon, line) == 0){
+        printf("\nline: %d Test successfull, expected %.10f got %.10f", line, b,a);
+    } else {
+        printf("\nline: %d Test failed, expected %.10f got %.10f",line, b,a);
+    }
+}
